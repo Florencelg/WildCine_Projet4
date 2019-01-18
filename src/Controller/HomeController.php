dc90eb483b202bdc5d3ba9fa834d407bdbd8d875
Florence LG <flo.lg.gautier@gmail.com>
2019-01-18 11:05:16 +0100

    #2: création du home controller, home index et layoout

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder22
+ * Date: 17/01/19
+ * Time: 22:53
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
